6ca11080a0e257741e3d87512748dd3ab500ba88
Ankit Kumar Soni <ankit31ks04@gmail.com>
2025-08-19 00:42:43 +0530

    changed the repo structure to different repos

--- api/utils/error.js
+++
@@ -1,8 +0,0 @@
-// we can create a custom error class to handle errors in a more structured way
-
-export const createError = (status, message) => {
-  const err = new Error();
-  err.status = status;
-  err.message = message;
-  return err;
-};
